4f15647eaf14ce2350126930d5cc39575a06a37c
elirex <sheng79225@gmail.com>
2025-02-26 02:24:40 +0800

    add OS check for set up on the different OS

--- shell_config.sh
+++ shell_config.sh
@@ -1,21 +1,24 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
 OS_TYPE=`uname`
 SHELL_TYPE=`ps -o comm= $$`
+
+if [[ "$OS_TYPE" = "Darwin" ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 alias vi='vim'
 alias py3='python3'
 alias py2='python2'
+alias ll='ls -alF'
 
 # The OS is Mac OS X
 if [[ "$OS_TYPE" = "Darwin" ]]; then
-    alias ll='ls -alF'
     export CLICOLOR='true'
     export TERM="xterm-256color"
     export LSCOLORS=Exfxcxdxxxegedabagacad
 fi
 
-if [[ "$SHELL_TYPE" = "-zsh"  || "$SHELL_TYPE" = "/bin/zsh" ]]; then
+if [[ "$SHELL_TYPE" = "-zsh"  || "$SHELL_TYPE" = "/bin/zsh" || "$SHELL_TYPE" =
+"zsh" ]]; then
     source ~/.terminal/zsh/zsh_config.zsh
 else
     source ~/.terminal/bash/bash_config.bash
